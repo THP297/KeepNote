eb43de074ae8675a290bec9c9ca0edf4832ccb45
Phu Tran <hensysppl@gmail.com>
2023-06-15 16:01:22 +0700

    asdqw

--- build_files.sh
+++ build_files.sh
@@ -1,11 +1,2 @@
-#!/bin/bash
-# Install Pipenv
-pip install pipenv
-
-# Activate virtual environment
-pipenv install --dev
-
-# Run build commands
-pipenv run python manage.py makemigrations --noinput
-pipenv run python manage.py migrate --noinput
-pipenv run python manage.py collectstatic --noinput --clear
+pip install -r requirements.txt 
+python3.9 manage.py collectstatic
