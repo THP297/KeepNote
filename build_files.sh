5a43cd19f27cb578514b52a5a652303a389e3e28
Phu Tran <hensysppl@gmail.com>
2023-06-15 15:51:39 +0700

    sixteenth change

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,5 @@
-python3.9 -m pip install -r requirements.txt
+python3.9 pipenv shell
+python3.9 -m pipenv install -r requirements.txt
 
 python3.9 manage.py makemigrations --noinput
 python3.9 manage.py makemirations --noinput
