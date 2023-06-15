a6ba0cf030a9dfd402820c7d9badc72297804d70
Phu Tran <hensysppl@gmail.com>
2023-06-15 15:24:25 +0700

    eight change

--- mykeep/settings.py
+++ mykeep/settings.py
@@ -129,7 +129,6 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS= [
     os.path.join(BASE_DIR,"static"),
 ]
-STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles_build')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 LOGIN_REDIRECT_URL = 'home'
