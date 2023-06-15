747ad8082a48451140fd5316511df0e74b6dbb11
Phu Tran <hensysppl@gmail.com>
2023-06-15 16:43:26 +0700

    qweqwe

--- mykeep/settings.py
+++ mykeep/settings.py
@@ -93,6 +93,13 @@ WSGI_APPLICATION = 'mykeep.wsgi.application'
 #     }
 # }
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
 
 
 # Password validation
